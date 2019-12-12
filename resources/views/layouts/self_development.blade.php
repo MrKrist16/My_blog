58996594715c9b5245d05cb2169bc00b6e1d42ef
Sergii Kirnosov <kirnosov082@gmail.com>
2019-12-12 16:51:51 +0200

    Remove folder 'public' in gitignore and finish webpage self-development

--- resources/views/layouts/self_development.blade.php
+++ resources/views/layouts/self_development.blade.php
@@ -22,40 +22,61 @@
 
     <div class="intro-self-development">
       <div class="mask rgba-black-strong" style="height: auto; min-height: 100vh;">
-        <div class="div-self-development">
-          <div class="blur">
+        <div class="container-self-development columns-self-development">
+          <div class="hvrbox">
             <a href="">
-              <img src="{{ asset('storage/uploads/1.jpg') }}">
+              <img src="{{ asset('storage/uploads/1.jpg') }}" class="hvrbox-layer_bottom">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
-          <div class="blur">
+          <div class="hvrbox">
             <a href="">
               <img src="{{ asset('storage/uploads/2.jpg') }}">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
-          <div class="blur">
+          <div class="hvrbox">
             <a href="">
               <img src="{{ asset('storage/uploads/3.jpg') }}">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
-          <div class="blur">
+          <div class="hvrbox">
             <a href="">
               <img src="{{ asset('storage/uploads/4.jpg') }}">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
-          <div class="blur">
+          <div class="hvrbox">
             <a href="">
               <img src="{{ asset('storage/uploads/5.jpg') }}">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
-          <div class="blur">
+          <div class="hvrbox">
             <a href="">
               <img src="{{ asset('storage/uploads/6.jpg') }}">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
-          <div class="blur">
+          <div class="hvrbox">
             <a href="">
               <img src="{{ asset('storage/uploads/7.jpg') }}">
+              <div class="hvrbox-layer_top">
+                <div class="hvrbox-text">Download</div>
+              </div>
             </a>
           </div>
         </div>
